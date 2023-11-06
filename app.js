b071599e49b6051b5c88fa120d6fd4ebbbad9184
KostasBuilts <kkir06@gmail.com>
2023-11-06 14:54:46 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const path = require("path")
+const mysql = require("mysql");
+const app = express();
+const dotenv = require("dotenv").config();
+const cookieParser = require("cookie-parser");
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json());
+app.use(cookieParser());
+app.set('view engine', 'html');
+const db = mysql.createConnection({
+    host: 'localhost',
+    //port: 3306,
+    user: "kostas",
+    password: "kostas2006",
+    database: "students"
+});
+
+db.connect((err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("MYSQL CONNECTED")
+    }
+})
+// Define Routes
+app.use('/', require('./routes/pages'));
+app.use('/auth', require('./routes/auth'));
+
+app.listen(8088)
